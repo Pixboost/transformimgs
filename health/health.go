8b52da3f3fafdfaf8594ae363335223422e7db2b
Dmitry Pokidov <dooman87@gmail.com>
2016-01-12 08:06:54 +1100

    FIX: health check test

--- health/health.go
+++ health/health.go
@@ -1,14 +1,13 @@
 //Provides health check
 package health
+
 import (
+	"fmt"
 	"net/http"
-//	"fmt"
 )
 
 //Returns OK string.
 //Shows only if service accessible or not.
 func Health(resp http.ResponseWriter, req *http.Request) {
-	http.Error(resp, "Error", 500)
-	//fmt.Fprintf(resp, "OK")
+	fmt.Fprintf(resp, "OK")
 }
-
