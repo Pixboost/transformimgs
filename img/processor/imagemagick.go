a27a85c8fa9698c19771763ef046507822f361eb
Dmitry Pokidov <dooman87@gmail.com>
2023-10-08 22:16:06 +1100

    Using jxl:effort 8 instead of 9 cause 9 is too slow

--- img/processor/imagemagick.go
+++ img/processor/imagemagick.go
@@ -481,7 +481,7 @@ func getOutputFormat(src *img.Info, target *img.Info, supportedFormats []string)
 func getConvertFormatOptions(source *img.Info) []string {
 	var opts []string
 	if source.Illustration {
-		opts = append(opts, "-define", "webp:lossless=true", "-quality", "100", "-define", "jxl:effort=9")
+		opts = append(opts, "-define", "webp:lossless=true", "-quality", "100", "-define", "jxl:effort=8")
 	}
 	if source.Format != "GIF" {
 		opts = append(opts, "-define", "webp:method=6")
