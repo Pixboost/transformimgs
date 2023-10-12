dc26e39ae22aee43d99c506241f6ad0cc957c2c9
Dmitry Pokidov <dooman87@gmail.com>
2023-10-13 07:29:59 +1100

    Setting JXL effort for lossy compression to 7

--- img/processor/imagemagick.go
+++ img/processor/imagemagick.go
@@ -485,6 +485,8 @@ func getConvertFormatOptions(source *img.Info) []string {
 	var opts []string
 	if source.Illustration {
 		opts = append(opts, "-define", "webp:lossless=true", "-quality", "100", "-define", "jxl:effort=9")
+	} else {
+		opts = append(opts, "-define", "jxl:effort=7")
 	}
 	if source.Format != "GIF" {
 		opts = append(opts, "-define", "webp:method=6")
