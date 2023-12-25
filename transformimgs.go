df9ecbe1a3ceb0e13fd4d4dd0c18d1811e31bf75
Dmitry Pokidov <dooman87@gmail.com>
2023-12-25 20:29:15 +1100

    Added doc for the root package

---
+++ transformimgs.go
@@ -0,0 +1,11 @@
+// Package transformimgs provides an easy way to add
+// image transformations into your web applications.
+//
+// Package provides simple API that manipulating images
+// and deliver them using next generation formats, including
+// AVIF and JpegXL.
+//
+// You could use the package to plugin into the existing
+// Go web application using img.Service.GetRouter method or
+// deploy as a standalone web app using docker image.
+package transformimgs
